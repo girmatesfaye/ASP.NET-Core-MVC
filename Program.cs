b367255516cba50ac7967e96056cd00338bf8272
girmatesfaye <girmaglory@gmail.com>
2026-02-27 03:16:10 -0800

    work with seed data

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
+using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 if (builder.Environment.IsDevelopment())
 {
@@ -13,6 +16,7 @@ else
     builder.Services.AddDbContext<MvcMovieContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMvcMovieContext")));
 }
+
 // builder.Services.AddDbContext<MvcMovieContext>(options =>
 //     options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
@@ -27,6 +31,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
 }
 
 app.UseHttpsRedirection();
