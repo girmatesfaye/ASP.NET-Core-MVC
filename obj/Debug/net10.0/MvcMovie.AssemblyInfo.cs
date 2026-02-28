f48d5f30845f101cdbc4af13f7fdfc42df3e801a
girmatesfaye <girmaglory@gmail.com>
2026-02-28 03:14:00 -0800

    Update AssemblyInfo with new informational version and cache inputs

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36aff39a5b989b572f5ab40769ed6afc0b8a3a36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c44f5fc9459cca916244bf79b664ffea1de1f9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
